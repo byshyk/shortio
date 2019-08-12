b722a005899244d6708f4e5a93ad22f2c91fdb40
Yaraslau Byshyk <ya@shyk.by>
2019-08-12 20:32:29 +0300

    Refactoring core functionality. Add docstrings

--- shortio/builtins_wrapper.py
+++ shortio/builtins_wrapper.py
@@ -1,35 +1,60 @@
+"""Contains wrappers for builtins.
+
+Each wrapper is used to avoid context manager boilerplate.
+
+Attributes:
+    read_json: Wrapped ``json.load``.
+    write_json: Wrapped ``json.dump``.
+    read_pickle: Wrapped ``pickle.load``.
+    write_pickle: Wrapped ``pickle.dump``.
+
+"""
+
 import json
 import pickle
-from typing import Any, AnyStr
 
-from .utils import PathType
+from .utils import read_wrapper, write_wrapper
 
 
-def read(file: PathType, mode: str = 'r') -> AnyStr:
-    with open(file, mode) as f:
+def read(file, mode='r', **kwargs):
+    """Read and return content of the file.
+
+    Args:
+        file: Path-like object giving the pathname of the file to be opened.
+        mode: String giving the mode of the file to be opened. Default: 'r'.
+        **kwargs: Optional arguments that ``open`` takes.
+
+    Returns:
+        File content.
+
+    """
+    with open(file, mode, **kwargs) as f:
         return f.read()
 
 
-def write(file: PathType, s: AnyStr, mode: str = 'w') -> None:
-    with open(file, mode) as f:
-        f.write(s)
+def write(file, s, mode='w', **kwargs):
+    """Read and return content of the file.
+
+    Args:
+        file: Path-like object giving the pathname of the file to be opened.
+        s: Content to be written.
+        mode: String giving the mode of the file to be opened. Default: 'w'.
+        **kwargs: Optional arguments that ``open`` takes.
+
+    Returns:
+        The number of characters/bytes (according to mode) written.
+
+    """
+    with open(file, mode, **kwargs) as f:
+        return f.write(s)
 
 
-def read_json(file: PathType) -> Any:
-    with open(file, 'r') as f:
-        return json.load(f)
+read_json = read_wrapper(json.load, mode='r')
+write_json = write_wrapper(json.dump, mode='w')
 
+read_pickle = read_wrapper(pickle.load, mode='rb')
+write_pickle = write_wrapper(pickle.dump, mode='wb')
 
-def write_json(file: PathType, obj: Any) -> None:
-    with open(file, 'w') as f:
-        json.dump(obj, f)
-
-
-def read_pickle(file: PathType) -> Any:
-    with open(file, 'rb') as f:
-        return pickle.load(f)
-
-
-def write_pickle(file: PathType, obj: Any) -> None:
-    with open(file, 'wb') as f:
-        pickle.dump(obj, f)
+__all__ = ['read', 'write',
+           'read_json', 'write_json',
+           'read_pickle', 'write_pickle']
--- shortio/yaml_wrapper.py
+++ shortio/yaml_wrapper.py
@@ -1,15 +1,28 @@
+"""Contains wrappers for yaml.
+
+Each wrapper is used to avoid context manager boilerplate.
+
+By default this module uses ``yaml.FullLoader`` for reading.
+See https://msg.pyyaml.org/load.
+
+Attributes:
+    read_yaml: Wrapped ``yaml.load``.
+    write_yaml: Wrapped ``yaml.dump``.
+    read_all_yaml: Wrapped ``yaml.load_all``.
+    write_all_yaml: Wrapped ``yaml.dump_all``.
+
+"""
+
 import yaml
-from typing import Any
 
-from .utils import PathType
-
-
-def read_yaml(file: PathType) -> Any:
-    with open(file, 'r') as f:
-        # TODO Add feature to use different loaders.
-        return yaml.full_load(f)
+from .utils import read_wrapper, write_wrapper
 
 
-def write_yaml(file: PathType, obj: Any) -> None:
-    with open(file, 'w') as f:
-        yaml.dump(obj, f)
+read_yaml = read_wrapper(yaml.load, mode='r', Loader=yaml.FullLoader)
+write_yaml = write_wrapper(yaml.dump, mode='w')
+
+read_all_yaml = read_wrapper(yaml.load_all, mode='r', Loader=yaml.FullLoader)
+write_all_yaml = write_wrapper(yaml.dump_all, mode='w')
+
+__all__ = ['read_yaml', 'write_yaml',
+           'read_all_yaml', 'write_all_yaml']
